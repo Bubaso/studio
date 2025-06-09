5a1fcd66ea89fe8072a38c1a360cfde8a4be8bb9
Burhan Başoğlu <burhanbasoglu4@gmail.com>
2025-06-09 23:33:35 +0000

    sergilelen ürünlerin konteynırları sağ ve solda ekranın sonuna yapışık h

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -11,7 +11,7 @@ export default function MainLayout({
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow container py-8">{children}</main>
+      <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8">{children}</main>
       <Footer />
       <Toaster />
     </div>
