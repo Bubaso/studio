b49719f502e97b5ead8afe5211b3321caa9320db
Burhan Başoğlu <burhanbasoglu4@gmail.com>
2025-06-09 21:46:15 +0000

    signin sayfasında yer alan ''Auth Layout (Ultra Basit)!! ifadesini kaldı

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -5,8 +5,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div>
-      <h1>Auth Layout (Ultra Basit)</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
       {children}
     </div>
   );
