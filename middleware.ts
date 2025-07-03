340fdb231a2d1a8653b62fb2313ce95c359a4a71
Burhan Başoğlu <burhanbasoglu4@gmail.com>
2025-07-03 02:04:05 +0000

    Bu adıma geçmeden önce bir sorum var: ben ilk 3 aşamayı tamamladığımızda

--- middleware.ts
+++ middleware.ts
@@ -2,15 +2,13 @@ import createMiddleware from 'next-intl/middleware';
  
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['fr', 'en'],
+  locales: ['fr', 'en', 'tr'],
  
   // Used when no locale matches
   defaultLocale: 'fr'
 });
  
 export const config = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/_next/static`, `/_next/image`
-  // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: ['/((?!api|_next/static|_next/image|.*\\..*).*)']
+  // Match only internationalized pathnames
+  matcher: ['/', '/(fr|en|tr)/:path*']
 };
