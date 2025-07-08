bcb40a489575ee7379f5a2493d78b8aa853a942a
Burhan Başoğlu <burhanbasoglu4@gmail.com>
2025-07-08 01:34:47 +0000

    butonları alt navigasyon barının üst çizgisini ortalayacak şekilde yükse

--- src/components/layout/bottom-tab-navigator.tsx
+++ src/components/layout/bottom-tab-navigator.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import Link from 'next/link';
@@ -15,7 +14,7 @@ export function BottomTabNavigator() {
 
     return (
         <nav className="md:hidden fixed bottom-0 left-0 right-0 h-16 bg-background border-t z-40">
-            <div className="flex justify-around items-center h-full">
+            <div className="flex justify-center items-center h-full gap-8">
                 
                 {/* Explorer Button */}
                 <Link href="/browse" className="flex flex-col items-center justify-center text-center -mt-8">
