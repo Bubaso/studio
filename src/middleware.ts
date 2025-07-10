18310a66f9b2774451a13b98fc8d38ddabb8d486
Burhan Başoğlu <burhanbasoglu4@gmail.com>
2025-07-10 22:24:50 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,20 @@
 
 import createMiddleware from 'next-intl/middleware';
+import {NextRequest} from 'next/server';
  
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ['fr', 'en', 'tr'],
- 
-  // Used when no locale matches
-  defaultLocale: 'fr'
-});
+export default async function middleware(request: NextRequest) {
+  // This is the default middleware configured for `next-intl`
+  const handleI18nRouting = createMiddleware({
+    locales: ['fr', 'en', 'tr'],
+    defaultLocale: 'fr'
+  });
+  const response = handleI18nRouting(request);
+  return response;
+}
  
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(fr|en|tr)/:path*']
+  // Skip all paths that should not be internationalized. This includes
+  // a positive lookahead to exclude API routes, Next.js system files,
+  // and paths with file extensions.
+  matcher: ['/((?!api|_next|.*\\..*).*)']
 };
